c162a62deb1e9f996161bcebf4fba3fd244c3768
am-faith <bluetooth_smart>
2014-07-18 14:15:08 +0800

    add
    add

---
+++ gen/com/wutl/ble/tools/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wutl.ble.tools;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/wutl/ble/tools/HelpActivity.java
@@ -0,0 +1,22 @@
+package com.wutl.ble.tools;
+
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_help);
+
+	}
+
+
+
+
+
+}
